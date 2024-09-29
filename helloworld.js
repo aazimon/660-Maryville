6ee9a543b6154cb49ae51b46f8a93267c0a29996
Gary <gary@deken.net>
2024-09-28 20:03:02 -0500

    minor change

--- helloworld.js
+++ helloworld.js
@@ -9,7 +9,8 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.write('<html><head><title>Company Portal</title></head>')
-   response.end('<body><h1>The Company Portal</h1><h2>Greetings to all Customers</h2></body></html>\n')
+   response.write('<body><h1>The Company Portal</h1><h2>Greetings to all Customers</h2>')
+   response.end('</body></html>\n')
 }).listen(3333)
 
 // Console will print the message
