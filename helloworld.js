3539f5aed9f1c4253341cc85591fc115837e497c
Gary <gary@deken.net>
2024-09-28 20:00:31 -0500

    Minor update

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,8 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/html'})
 
    // Send the response body as "Hello World"
-   response.end('<html><head><title>Company Portal</title></head><body><h1>The Company Portal</h1><h2>Greetings to all Customers</h2></body></html>\n')
+   response.write('<html><head><title>Company Portal</title></head>')
+   response.end('<body><h1>The Company Portal</h1><h2>Greetings to all Customers</h2></body></html>\n')
 }).listen(3333)
 
 // Console will print the message
